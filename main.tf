5bd718411c5b84bddd86512c2488a500cb8c7b31
Abdallah <abdallahhegazy98@gmail.com>
2026-07-10 15:27:23 +0300

    Third

--- main.tf
+++ main.tf
@@ -80,6 +80,14 @@ resource "aws_security_group" "allow_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Flask"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
